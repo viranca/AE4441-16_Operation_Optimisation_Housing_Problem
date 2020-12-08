d80612e832977a1af9456846e181ecec983452fa
ka40 <ka40@users.noreply.github.com>
2020-12-08 12:25:34 +0100

    Verification Scenarios

---
+++ Verification_scenario.py
@@ -0,0 +1,7 @@
+from Student_dataset import Student_dataset
+from House_dataset import House_dataset
+
+if __name__ == '__main__':
+    students = Student_dataset(20)
+    houses = House_dataset(4)
+
