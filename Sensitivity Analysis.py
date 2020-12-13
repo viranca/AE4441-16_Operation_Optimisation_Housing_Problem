925d847e965ef1b461f8d93ca9cfbbd5e6b941ee
viranca <37591728+viranca@users.noreply.github.com>
2020-12-13 16:10:34 +0100

    sensitivity analysis base model

--- Sensitivity Analysis.py
+++ Sensitivity Analysis.py
@@ -1,84 +1,99 @@
 
-
-
-
-
 from House_dataset import House_dataset
 from Student_dataset import Student_dataset
 from Model_generator import Model_generator
 
+import sys
+
+def print_statusline(msg: str):
+    last_msg_length = len(print_statusline.last_msg) if hasattr(print_statusline, 'last_msg') else 0
+    print(' ' * last_msg_length, end='\r')
+    print(msg, end='\r')
+    sys.stdout.flush()  # Some say they needed this, I didn't.
+    print_statusline.last_msg = msg
 
 
-statistical_properties =              {"room_count": {"mu": 5000,
-                                                      "sigma": 1},
+number_of_students = 100
+number_of_houses = 15
+number_of_montecarlo_iterations = 1000
 
-                                       "rent_per_room": {"mu": 5000,
-                                                         "sigma": 100},
+statistical_properties = {"room_count": {"mu": 3,
+                                        "sigma": 1},
 
-                                       "location": {"mu": 5000,
-                                                    "sigma": 25}}
+                          "rent_per_room": {"mu": 600,
+                                            "sigma": 100},
 
-Student_dataset1 = Student_dataset(100)
-House_dataset1 = House_dataset(15)
-
-
-model = Model_generator(Student_dataset1, House_dataset1)
-model.output_to_lp()
-model.optimize()
-
-solution = []   
-for v in model.getVars():
-     solution.append([v.varName,v.x])
-     
-#model.printAttr('X')
-
-
-#%%
-# print(model.house_dataset.statistical_properties)
-
-# model.house_dataset.statistical_properties = {"room_count": {"mu": 5000,
-#                                                       "sigma": 1},
-
-#                                        "rent_per_room": {"mu": 5000,
-#                                                          "sigma": 100},
-
-#                                        "location": {"mu": 5000,
-#                                                     "sigma": 25}}
+                          "location": {"mu": 50,
+                                       "sigma": 25},
+                         
+                          "budget_min": {"mu": 300,
+                                         "sigma": 100}}
 
 
 
-# print(model.house_dataset.statistical_properties)
+def run_model(number_of_students, number_of_houses, statistical_properties):
+    Student_dataset1 = Student_dataset(number_of_students, statistical_properties=statistical_properties)
+    House_dataset1 = House_dataset(number_of_houses, statistical_properties=statistical_properties)
 
-# Student_dataset2 = Student_dataset(100)
-# House_dataset2 = House_dataset(15)
+    #postprocesss script House_dataset1
+    #dutch international
 
-# model = Model_generator(model.student_dataset, model.house_dataset)
-# model.output_to_lp()
-# model.optimize()
-
-#%%
-
-
-from House_dataset import House_dataset
-from Student_dataset import Student_dataset
-from Model_generator import Model_generator
-
-
-#do this for varying parameters 
-complete_solution = []
-for MC_i in range(1000):
-    model = Model_generator(Student_dataset(100), House_dataset(15))
+ 
+    model = Model_generator(Student_dataset1, House_dataset1)
     model.output_to_lp()
-    model.optimize()  
-    solution_i = []   
-    for v in model.getVars():
-        solution.append([v.varName,v.x])
-    complete_solution.append(solution_i)
-    
+    model.optimize()
+
+    # #print all x_ij:
+    # solution = []   
+    # for v in model.model.getVars():
+    #       solution.append([v.varName,v.x])
+    #print(solution)
+    return model.model.ObjVal
 
 
-#statistical tests on the complete_solution, and between complete_solutions with different input.
 
+solutions_base_model = []
+for i in range(number_of_montecarlo_iterations):
+    solutions_base_model.append(run_model(number_of_students, number_of_houses, statistical_properties))
+    #print('\x1b[2K' , "iteration {}".format(i) +'\r',  end ='')
+    print_statusline('iteration: ' + str(i))
+
+print(solutions_base_model)    
+
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+
+
+
+
+
+
+
+
+##statistical tests on the complete_solution, and between complete_solutions with different input.
 
 
 
